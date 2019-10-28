6c8826f0e422c5d4df70d40aec0c136620d7e6cf
jordan <jordanbouget@gmail.com>
2019-10-28 14:12:26 +0100

    Ajout de la méthode calculerMargeProduit : récupère chaque produit et calcule la marge

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -16,7 +16,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Api( description="API pour es opérations CRUD sur les produits.")
@@ -103,6 +105,16 @@ public class ProductController {
         return productDao.chercherUnProduitCher(400);
     }
 
+    //Récupère chaque produit et calcule la marge
+    @GetMapping(value = "/AdminProduits")
+    public Map<Product, Integer> calculerMargeProduit(){
+        Map<Product, Integer> listeMargeProduits= new HashMap<>();
+        List<Product> produits = productDao.findAll();
+        for (Product produit : produits){
+            listeMargeProduits.put(produit, (produit.getPrix() - produit.getPrixAchat()));
+        }
+        return listeMargeProduits;
+    }
 
 
 }
